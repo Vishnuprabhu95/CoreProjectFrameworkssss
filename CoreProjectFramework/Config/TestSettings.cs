51dfa3076d1c5b2e8a7fc1eef8d958d188cb9f25
Vishnuprabhu95 <itsvishnuemail@gmail.com>
2020-02-26 22:00:52 +0530

    Add project files.

---
+++ CoreProjectFramework/Config/TestSettings.cs
@@ -0,0 +1,19 @@
+﻿using AutoFramework.Base;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoFramework.Config
+{
+    [JsonObject("testSettings")]
+    public class TestSettings
+    {
+        [JsonProperty("aut")]
+        public string AUT { get; set; }
+
+        [JsonProperty("browser")]
+        public string browser { get; set; }
+
+    }
+}
