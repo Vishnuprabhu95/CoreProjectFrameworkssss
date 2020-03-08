85b7592efdad5719f7d763e80609ee72706055ec
Vishnuprabhu95 <itsvishnuemail@gmail.com>
2020-03-08 23:02:57 +0530

    added screenshot

--- AutomationTest/Hooks/HookInitialize.cs
+++ AutomationTest/Hooks/HookInitialize.cs
@@ -7,12 +7,14 @@ using OpenQA.Selenium;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Tracing;
 
 namespace AutomationTest
 {
@@ -204,5 +206,79 @@ namespace AutomationTest
             extent.Flush();
         }
 
+        [AfterStep]
+        public void AfterWebTest()
+        {
+            if (!ScenarioContext.Current.ScenarioExecutionStatus.ToString().Contains("OK"))
+            {
+                TakeScreenshot(DriverContext.Driver);
+            }
+        }
+
+        
+        private void TakeScreenshot(IWebDriver driver)
+        {
+            try
+            {
+                string fileNameBase = string.Format("error_{0}_{1}_{2}",
+                                                    FeatureContext.Current.FeatureInfo.Title.ToIdentifier(),
+                                                    ScenarioContext.Current.ScenarioInfo.Title.ToIdentifier(),
+                                                    DateTime.Now.ToString("yyyyMMdd_HHmmss"));
+
+                var artifactDirectory = Path.Combine(Directory.GetCurrentDirectory(), "testresults");
+                if (!Directory.Exists(artifactDirectory))
+                    Directory.CreateDirectory(artifactDirectory);
+
+                string pageSource = driver.PageSource;
+                string sourceFilePath = Path.Combine(artifactDirectory, fileNameBase + "_source.html");
+                File.WriteAllText(sourceFilePath, pageSource, Encoding.UTF8);
+                Console.WriteLine("Page source: {0}", new Uri(sourceFilePath));
+
+                ITakesScreenshot takesScreenshot = driver as ITakesScreenshot;
+
+                if (takesScreenshot != null)
+                {
+                    var screenshot = takesScreenshot.GetScreenshot();
+
+                    string screenshotFilePath = Path.Combine(artifactDirectory, fileNameBase + "_screenshot.png");
+
+                    screenshot.SaveAsFile(screenshotFilePath, OpenQA.Selenium.ScreenshotImageFormat.Png);
+
+                    Console.WriteLine("Screenshot: {0}", new Uri(screenshotFilePath));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while taking screenshot: {0}", ex);
+            }
+
+            //try
+            //{
+            //    //Initialize Extent report before test start
+            //    string _logFileName = string.Format("{0:yyyy-dd-M--HH-mm-ss}", DateTime.Now);
+
+            //    string dir = @"C:\\dev\\Screenshots";
+
+            //    if (Directory.Exists(dir))
+            //    {
+
+            //    }
+            //    else
+            //    {
+            //        Directory.CreateDirectory(dir);
+
+            //    }
+
+            //    Screenshot file = ((ITakesScreenshot)DriverContext.Driver).GetScreenshot();
+            //    file.SaveAsFile(@"C:\\dev\\Screenshots\\FS--"+ ScenarioContext.Current.ScenarioInfo.Title+"--"+_logFileName+".png");
+            //    //Screenshot file = ((ITakesScreenshot)driver).GetScreenshot();
+            //    //byte[] image = file.AsByteArray;
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Error while taking screenshot: {0}", ex);
+            //}
+        }
     }
 }
